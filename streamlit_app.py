eddfa9127f2aafe2e5b62ee31158f1b499c38367
N Hameleers <niels.hameleers@gmail.com>
2024-11-27 13:18:37 +0000

    add interactive example

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,18 @@ st.title('MAI Dashboard')
 
 df = pd.read_csv('https://raw.githubusercontent.com/LUCE-Blockchain/Databases-for-teaching/refs/heads/main/Framingham%20Dataset.csv')
 
-# st.dataframe(df.head())
+'## Project data'
+# look at the data
+st.dataframe(df)
 
-# add matplotlib figure
+'## Matplotlib figure'
 fig, ax = plt.subplots()
 ax.scatter(df.AGE, df.SYSBP, alpha=0.3)
 st.pyplot(fig)
+
+'## Adding interactivity'
+yvar = st.selectbox(label='Enter y-axis variable', options=['SYSBP'])
+fig, ax = plt.subplots()
+ax.scatter(df.AGE, df[yvar], alpha=0.3)
+st.pyplot(fig)
+
